f234f479a089f82f1131294568f906e37cea7483
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2020-10-25 10:18:58 +0530

    Create solution.kt

---
+++ Day 16/solution.kt
@@ -0,0 +1,10 @@
+  
+fun main(args: Array<String>) {
+    val inputString = readLine()
+    try {
+        val integer = Integer.parseInt(inputString)
+        println(integer)
+    } catch (numberFormatException: NumberFormatException) {
+        println("Bad String")
+    }
+}
